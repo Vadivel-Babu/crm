6933b1ec85014efa71ec8c4ee423ca5e7c17ffd9
Vadivel-Babu <vadivelbabu1996@gmail.com>
2025-11-21 17:13:07 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>hello client</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "hi" });
+});
+
+app.listen(PORT, () => {
+  console.log(`app listening to http://localhost:${PORT}`);
+});
