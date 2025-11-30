0b85571281a295fa32cbbd677939ad8bf6a31022
Vadivel-Babu <vadivelbabu1996@gmail.com>
2025-11-30 19:23:19 +0530

    url changed

--- crm/src/api/api.js
+++ crm/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://crm-jryf.onrender.com/api",
 });
 
 api.interceptors.response.use(
